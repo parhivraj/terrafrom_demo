9f395d67f57df18ec9ae4d0d6b53c71f3e4c86ca
Your Name <you@example.com>
2023-09-16 04:36:23 +0000

    created main.tf

---
+++ main.tf
@@ -0,0 +1,26 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+#Resours Block
+resource "aws_instance" "app_server" {
+  ami           = "ami-053b0d53c279acc90"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
+
